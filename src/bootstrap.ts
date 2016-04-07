d1b165ddd6d0159f15a03d0903bbd9bfb5d25d74
Shinsuke Nishio <nishio@densan-labs.net>
2016-04-08 02:31:38 +0900

    add

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,10 +1,23 @@
 /// <reference path="../typings/main.d.ts" />
 
-namespace HyperImposer {
+module HyperImposer {
   export class Bootstrap {
 
-    public main() {
-      console.log('Hello');
+    private debug = false;
+
+    constructor(debug: boolean) {
+      this.debug = debug;
+      if (this.isDebugMode()) {
+        console.log('Initialize HyperImposer');
+      }
+    }
+
+    public isDebugMode() {
+      return this.debug;
     }
   }
 }
+
+(function() {
+  (<any>window).HyperImposer = HyperImposer.Bootstrap;
+})();
