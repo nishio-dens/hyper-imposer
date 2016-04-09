7df452208e0da3a21afd92b956326b6766b60a2f
Shinsuke Nishio <nishio@densan-labs.net>
2016-04-09 17:11:46 +0900

    add

--- src/renderer/renderer.ts
+++ src/renderer/renderer.ts
@@ -7,20 +7,21 @@ namespace HyperImposer {
     private canvasWidth: number;
     // レンダリングするCanvasの高さ
     private canvasHeight: number;
-    // canvasのscale 大きいcanvasにrenderして縮めることで見た目を綺麗にする
-    private canvasScale: number;
     // 仮想canvasの幅
     private virtualCanvasWidth: number;
     // 仮想canvasの高さ
     private virtualCanvasHeight: number;
 
-    constructor(canvasWidth: number, canvasHeight: number, canvasScale: number) {
+    constructor(canvasWidth: number, canvasHeight: number, aspectRatio:any) {
       this.canvasWidth = canvasWidth;
       this.canvasHeight = canvasHeight;
       this.canvasScale = canvasScale;
 
       this.virtualCanvasWidth = this.canvasScale * this.canvasWidth;
       this.virtualCanvasHeight = this.canvasScale * this.canvasHeight;
+    }
+
+    public setCanvasSize(canvasWidth: number, canvasHeight: number) {
     }
 
     public render() {
