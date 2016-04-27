74903baf239742a492d70c098b0915b8b07f8a28
Shinsuke Nishio <nishio@densan-labs.net>
2016-04-28 02:14:39 +0900

    add

--- src/captions/captionXmlParser.ts
+++ src/captions/captionXmlParser.ts
@@ -1,6 +1,12 @@
 /// <reference path="../../typings/main.d.ts" />
 
 export class CaptionXmlParser {
+  private currentHid;
+
+  public constructor() {
+    this.currentHid = new Date().getTime();
+  }
+
   public parseXml(text: string, parents = []) {
     var parser = new DOMParser();
     var dom = parser.parseFromString("<ROOT>" + text + "</ROOT>", "text/xml");
@@ -16,6 +22,8 @@ export class CaptionXmlParser {
     if (children.length > 0) {
       for (var i = 0; i < children.length; i++) {
         var n = children[i];
+        this.setNodeIdIfNotExists(n);
+
         var attributes = [];
         if (n.attributes) {
           for (var j = 0; j < n.attributes.length; j++) {
@@ -25,6 +33,7 @@ export class CaptionXmlParser {
           }
         }
         var p = innerNodes.concat({
+          nodeId: n.id.toString(),
           nodeType: n.nodeName,
           nodeValue: n.nodeValue,
           attributes: attributes
@@ -35,6 +44,7 @@ export class CaptionXmlParser {
     } else {
       results = [
         {
+          nodeId: node.id.toString(),
           nodeType: node.nodeName,
           nodeValue: node.nodeValue,
           innerNodes: innerNodes
@@ -43,5 +53,12 @@ export class CaptionXmlParser {
     }
 
     return results;
+  }
+
+  private setNodeIdIfNotExists(node) {
+    if (node.id === undefined || node.id === "") {
+      node.id = this.currentHid;
+      this.currentHid += 1;
+    }
   }
 }
